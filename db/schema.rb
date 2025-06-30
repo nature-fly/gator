04f2cb0b99a857b292763a97cc5fb2887faf13c1
sajeet17 <sajeetp@fetch.ly>
2025-06-30 14:49:40 +0545

    Generate scaffold for basic profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_20_090830) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_30_085540) do
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
